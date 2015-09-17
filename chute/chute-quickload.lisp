21654b14bec705afcef14b5b3fa45e6c31991354
Mark <evenson.not.org@gmail.com>
2015-09-17 14:44:50 +0200

    Quicklisp load script for system dependencies

---
+++ chute/chute-quickload.lisp
@@ -0,0 +1,9 @@
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(dolist (system '(:ironclad))
+  (ql:quickload system))
+
