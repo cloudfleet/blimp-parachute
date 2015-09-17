0f222f002aecbd56d49e5fc73acaa0d99eb3ad62
Mark <evenson.not.org@gmail.com>
2015-09-17 14:44:46 +0200

    Script to create initial data subvolume
    Assumes that the /opt/cloudfleet mount (or its ancestors) is an btrfs
    volume.

---
+++ setup/add-subvolume.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+path=/opt/cloudfleet/data
+path_copy=${path}.copy
+
+if [ -e ${path_copy} ]; then 
+   echo Location to backup cloudfleet data already exists at ${path_copy}
+   exit 1
+fi
+
+echo Stopping Cloudfleet services
+/opt/cloudfleet/engineroom/bin/stop-containers.sh
+
+mv $path $path_copy
+
+btrfs subvolume create /opt/cloudfleet/data
+rsync -avzP ${path_copy}/ ${path}/
+rm -rf ${path_copy}
+
+mkdir /opt/cloudfleet/.snapshots
+
+echo Starting Cloudfleet services
+/opt/cloudfleet/engineroom/bin/start-containers.sh
+
+
+
+
+
