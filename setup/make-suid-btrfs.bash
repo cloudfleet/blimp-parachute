1361188d037ce4a5ecdd52b4e1300550c11d63fd
Mark Evenson <x40@cloudfleet.io>
2015-09-22 15:27:04 +0200

    Make setuid btrfs creation more robust

--- setup/make-suid-btrfs.bash
+++ setup/make-suid-btrfs.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 # as root...
+if [ $(id -u) != 0 ]; then
+    echo Insufficient privileges to create setuid link
+    exit 1
+fi
+
+if [ -O btrfs ]; then
+    echo btrfs already exists
+    exit 1
+fi
+
 ln /sbin/btrfs btrfs
 chmod u+s btrfs
+echo Created setuid at  $PWD/btrfs 
 
