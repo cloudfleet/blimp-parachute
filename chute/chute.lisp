2b1067d0a971b411e6a24bad707a7449cded7253
Mark <easye@gmail.com>
2015-11-19 10:35:32 +0100

    Fix MAKE-NEW-DIRECTORY under ccl

--- chute/chute.lisp
+++ chute/chute.lisp
@@ -44,7 +44,11 @@
 
 (defun make-new-directory ()
   (ensure-directories-exist *blobs-directory*)
-  (let ((file (uiop/stream::get-temporary-file :directory *blobs-directory*)))
+  (let* ((directory
+          #+ccl
+           (subseq *blobs-directory* 0 (1- (length *blobs-directory*)))
+           #-ccl *blobs-directory*)
+          (file (uiop/stream::get-temporary-file :directory directory)))
     (delete-file file)
     (ensure-directories-exist
      (pathname (concatenate 'string (namestring file) "/")))))
