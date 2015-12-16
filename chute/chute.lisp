fa275118c97d14ef03d8cae131ac6b5fb8e6c7ba
Mark <evenson@panix.com>
2015-12-16 18:11:08 +0100

    Initial implementation of client as snapshot and transfer tasks
    <file:model/chute.n3> contains a draft of the model.

--- chute/chute.lisp
+++ chute/chute.lisp
@@ -6,16 +6,26 @@
   (if lparallel:*kernel*
       (warn "lparallel kernel unexpectedly present.")
       (setf lparallel:*kernel* (lparallel:make-kernel 3)))
-  (let ((backup-channel (lparallel:make-channel)))
+  (let ((channel (lparallel:make-channel)))
     (note "Performing single backup task.")
+    ;;; TODO implement Guardian, resubmit snapshot and transfer tasks as necessary
     (values
-     (lparallel:submit-task backup-channel #'backup)
-     (lparallel:submit-task backup-channel (lambda () (sleep 360) (note "Slept for an hour.  Whatsup?")))
-     backup-channel)))
+     backup-channel
+     (lparallel:submit-task backup-channel
+                            #'snapshot) 
+     (lparallel:submit-task backup-channel
+                            #'transfer)
+     (lparallel:submit-task backup-channel
+                            (lambda () (sleep 360) (note "Slept for an hour.  Whatsup?"))))))
 
-(defun backup ()
-  "Main task for backup.  To be run periodically on queue."
-  (let ((snapshot-path (snapshot)) ;; create snapshot
+
+(defun transfer ()
+  "Main task for transferring backups.  To be run periodically on queue."
+  (let ((snapshot-path (progn
+                         (unless (first (btrfs-snapshots))
+                           (warn "Transfer task found no snapshot to transfer.  Creating one.")
+                           (snapshot))
+                         (last (btrfs-snapshots))))
         (blob-path (uiop:temporary-directory)))
     (make-blob snapshot-path blob-path)     ;; serialize encrypted blob to path
     (prog1
