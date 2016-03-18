45d9ef6b57ca1161762255cc61df9e3d9ace36e6
Mark <evenson.not.org@gmail.com>
2016-03-18 08:50:07 +0000

    Structure information about transfers

--- chute/transfer-http.lisp
+++ chute/transfer-http.lisp
@@ -1,8 +1,33 @@
 (in-package :chute)
 
+(defclass transfer ()
+  ((snapshot
+    :accessor snapshot
+    :initarg :snapshot
+    :documentation "Local snapshot corresponding to this transfer.")
+   (blob-path
+    :accessor blob-path
+    :initform nil
+    :documentation "On-disk representation of snapshot as blob.")))
+
+(defclass transfer-http (transfer)
+   ((status
+    :accessor status
+    :initform nil
+    :documentation "Transfer status of blob.  One of nil, :transferring, or :done")))
+
 (defmethod transfer ((method (eql :http)) snapshot-path)
   (let ((blob-path (uiop:temporary-directory)))
     ;; serialize encrypted blob to path
     (make-blob snapshot-path blob-path)     
     (cloudfleet/transfer-blob blob-path)))
+
+(defun transcribe-transfers ()
+  (let (transfers)
+    (dolist (snapshot (btrfs-snapshots))
+      (push
+       (make-instance 'transfer-http
+                      :snapshot snapshot)
+       transfers)
+      transfers)))
 
