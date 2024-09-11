eef0a8a6196bb9f517d647be0a1b6496eacf8461
Mark Evenson <evenson.not.org@gmail.com>
2024-09-11 18:24:22 +0200

    CHUTE:SINK initial rsync implementation

--- chute/rsync.lisp
+++ chute/rsync.lisp
@@ -6,3 +6,12 @@
 (defclass rsync (fs)
   ((unimplemented)))
 
+;;; one-way rsync (potentially destructive remotely)
+(defun sink (local remote)
+  (warn "Attempting undebugged rsync from ~&<~a> to~&<~a>~%" local remote)
+  (values 
+   (uiop:run-program
+    '("rsync" "-avzP" local remote)
+    :output :string)))
+
+
